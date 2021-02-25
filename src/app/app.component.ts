9269714b2921e44e2d2693d6a24b53f73111d9b6
NEYWIL <uribewa1@gmail.com>
2021-02-24 23:22:31 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // ,
+  // styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Contador';
+  title: string = 'Contador App';
+
+  numero: number = 10;
+
+  // sumar() {
+  //   this.numero += 1;
+  // }
+
+  // restar() {
+  //   this.numero -= 1;
+  // }
+
+  base: number = 5;
+
+  acumular(valor: number) {
+    this.numero += valor;
+  }
 }
